792cc29a4baea280acb282cb60117b7fd90579b4
Hyunmin Jung <jung@hyunmin.dev>
2021-08-07 22:48:04 +0900

    feat: Add appbar with github and tistory link

---
+++ lib/views/common_view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class CommonView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hyunmindev'),
+        actions: [
+          IconButton(
+            iconSize: 32,
+            onPressed: () {
+              _launchURL('https://blog.hyunmin.dev');
+            },
+            tooltip: 'Tistory',
+            icon: Image.asset('tistory-logo-white.png'),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          IconButton(
+            iconSize: 32,
+            onPressed: () {
+              _launchURL('https://github.com/hyunmindev');
+            },
+            tooltip: 'Github',
+            icon: Image.asset('github-logo-white.png'),
+          ),
+          SizedBox(
+            width: 24,
+          ),
+          ThemeSwitch(),
+          SizedBox(
+            width: 24,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('test');
+        },
+      ),
+    );
+  }
+}
+
+class ThemeSwitch extends StatefulWidget {
+  _ThemeSwitchState createState() => _ThemeSwitchState();
+}
+
+class _ThemeSwitchState extends State<ThemeSwitch> {
+  bool _isDarkTheme = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: _isDarkTheme,
+        onChanged: (value) {
+          setState(() {
+            _isDarkTheme = value;
+            context.read<MyTheme>().setIsDarkTheme(value);
+          });
+        });
+  }
+}
+
+_launchURL(url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
